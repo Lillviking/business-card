529177b4a08cdb0b58aa7f9f1218b3b9ee08c45d
Frida Ulfves <frida_ulfves@hotmail.com>
2024-08-06 14:35:21 +0200

    method to calculate random id

---
+++ packages/backend/src/utils/randomId.js
@@ -0,0 +1,14 @@
+// src/utils/randomId.js
+
+function generateRandomId(length) {
+    const characters = '0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+    return result;
+  }
+  
+  module.exports = generateRandomId;
+  
